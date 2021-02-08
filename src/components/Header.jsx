17f19100d3d4a8be5bca72cbd6aaa45395e0e902
aaodeh <66892536+aaodeh@users.noreply.github.com>
2021-02-08 03:29:41 -0500

    added new google font for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Icon, InlineIcon } from "@iconify/react";
 import grinningCat from "@iconify-icons/twemoji/grinning-cat";
 import catWithWrySmile from "@iconify-icons/twemoji/cat-with-wry-smile";
 
-const Header = () => {
+const Header = (AddCat) => {
   return (
     <header className="header">
       <div
@@ -13,9 +13,14 @@ const Header = () => {
           padding: "5px"
         }}
       >
-        <Icon icon={catWithWrySmile} width={30} className="marker-icon" />
-        <span style={{ fontSize: "2rem" }}>Thunder Cats</span>
-        <Icon icon={catWithWrySmile} width={30} className="marker-icon" />
+        <div>
+          <Icon icon={catWithWrySmile} width={30} className="marker-icon" />
+          <span style={{ fontSize: "2rem" }}>Cat Collectables</span>
+          <Icon icon={catWithWrySmile} width={30} className="marker-icon" />
+        </div>
+        <span>
+          <button onClick={AddCat}>Add a Cat</button>
+        </span>
       </div>
     </header>
   );
